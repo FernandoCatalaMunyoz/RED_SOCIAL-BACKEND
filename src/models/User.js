1d766d3225b48fc39b3e9b43148f38303c2c5a31
Fernando <fernandocatalamunyoz@gmail.com>
2024-03-08 17:09:46 +0100

    feat: registro y login añadidos

--- src/models/User.js
+++ src/models/User.js
@@ -5,11 +5,11 @@ const UserSchema = new Schema(
   {
     name: {
       type: String,
-      required: false,
+      required: true,
     },
     email: {
       type: String,
-      required: false,
+      required: true,
       unique: true,
     },
     password: {
