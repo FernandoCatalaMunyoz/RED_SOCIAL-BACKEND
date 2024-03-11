66f74ca5ca0e067d9cd95fcc24b41d8fd437a55e
Fernando <fernandocatalamunyoz@gmail.com>
2024-03-11 19:20:43 +0100

    feat: enpoint ver perfil añadido

--- src/controllers/user.Controller.js
+++ src/controllers/user.Controller.js
@@ -1,9 +1,11 @@
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 import User from "../models/User.js";
 
+//ver todos los usuarios
 export const getUsers = async (req, res) => {
   try {
-    const users = await User.find({}).select("-password").exec();
+    const users = await User.find({}, "-password").exec();
 
     res.status(201).json({
       succes: true,
@@ -14,6 +16,26 @@ export const getUsers = async (req, res) => {
     res.status(500).json({
       succes: false,
       message: "Users cant be retrieved",
+    });
+  }
+};
+
+// ver perfil
+export const getProfile = async (req, res) => {
+  try {
+    const userId = req.tokenData.userId;
+
+    const user = await User.findById(userId).exec();
+
+    res.status(200).json({
+      succes: true,
+      message: "Profile retrieved succesfully",
+      data: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      succes: false,
+      message: "Profile cant be retrieved",
     });
   }
 };
--- src/routes/user.Routes.js
+++ src/routes/user.Routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/user.Controller.js";
+import { getProfile, getUsers } from "../controllers/user.Controller.js";
 import { auth } from "../middlewares/auth.js";
 import { isSuperAdmin } from "../middlewares/isSuperAdmin.js";
 
 const userRouter = Router();
 
 userRouter.get("/", auth, isSuperAdmin, getUsers);
+userRouter.get("/profile", auth, getProfile);
 
 export default userRouter;
