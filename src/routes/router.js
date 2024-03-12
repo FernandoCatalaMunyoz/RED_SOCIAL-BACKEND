a841ac5bc083be164a6eb085e1ce218c94d081a4
Fernando <fernandocatalamunyoz@gmail.com>
2024-03-12 17:23:49 +0100

    añado halethy para comprobar deploy

--- src/routes/router.js
+++ src/routes/router.js
@@ -8,4 +8,10 @@ const router = Router();
 router.use("/auth", authRouter);
 router.use("/users", userRouter);
 router.use("/post", postRouter);
+app.use("/api/healthy", (req, res) => {
+  res.status(200).json({
+    succes: true,
+    message: "Server is healthy",
+  });
+});
 export default router;
