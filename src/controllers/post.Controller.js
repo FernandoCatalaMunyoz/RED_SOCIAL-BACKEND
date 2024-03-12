f631158336433d9597ffa833ab402c37e4ac30cb
Fernando <fernandocatalamunyoz@gmail.com>
2024-03-12 09:28:10 +0100

    feat: Endpoint actualizar post por Id

--- src/controllers/post.Controller.js
+++ src/controllers/post.Controller.js
@@ -1,7 +1,6 @@
-// funcion crear post
-
 import Post from "../models/Post.js";
 
+// funcion crear post
 export const createPost = async (req, res) => {
   try {
     const description = req.body.description;
@@ -21,6 +20,54 @@ export const createPost = async (req, res) => {
     res.status(500).json({
       success: false,
       message: "Post cant be created",
+      error: error,
+    });
+  }
+};
+
+// funcion borrar post por id
+
+export const deletePostById = async (req, res) => {
+  try {
+    const postId = req.body.postId;
+    const deletePost = await Post.findByIdAndDelete(postId);
+
+    res.status(200).json({
+      success: true,
+      message: "Post deleted succesfully",
+      data: deletePost,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Post cant be deleted",
+      error: error,
+    });
+  }
+};
+
+export const updatePostById = async (req, res) => {
+  try {
+    const description = req.body.description;
+    const postId = req.params.id;
+
+    const postUpdated = await Post.findByIdAndUpdate(
+      postId,
+      {
+        description: description,
+      },
+      { new: true }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Post updated succesfully",
+      data: postUpdated,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Post cant be updated",
       error: error,
     });
   }
--- src/routes/post.Routes.js
+++ src/routes/post.Routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { createPost } from "../controllers/post.Controller.js";
+import {
+  createPost,
+  deletePostById,
+  updatePostById,
+} from "../controllers/post.Controller.js";
 import { auth } from "../middlewares/auth.js";
 
 const postRouter = Router();
 
 postRouter.post("/", auth, createPost);
+postRouter.delete("/:id", deletePostById);
+postRouter.put("/:id", updatePostById);
 
 export default postRouter;
